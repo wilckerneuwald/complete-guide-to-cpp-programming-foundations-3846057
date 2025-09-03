4337a5e13bfad364937d36100c926905d6955949
Wilcker Neuwald Schinestzki <wilcker.neuwaldschinestzki@zf.com>
2025-09-03 11:27:26 +0200

    Add hello world also with command line interface

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,16 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+
+int main(int argc, char* argv[]){
+    std::cout << "Hello World!" << std::endl;
+
+    for (int i=0; i < argc; i++) {
+        std::cout << "argv[" << i << "] = " << argv[i] << std::endl;
+    }
+
+    std::cout << std::endl;
+    return 0;
+}
